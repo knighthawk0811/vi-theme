3a205f84a096f052906da0d007de2edb0810a8ac
Joseph Neathawk <11905983+knighthawk0811@users.noreply.github.com>
2021-02-26 18:33:50 -0600

    update navigation and header/footer

--- header.php
+++ header.php
@@ -11,6 +11,12 @@
 
 @session_start();
 $_SESSION['timer_start'] = microtime(true);
+
+$display_header = true;
+if( in_array( 'blank-iframe', vi_theme_body_class() ) ){
+    $display_header = false;
+}
+
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -25,34 +31,28 @@ $_SESSION['timer_start'] = microtime(true);
 <body id="top" <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<?php
-$display_header = true;
-if( in_array( 'blank-iframe', vi_theme_body_class() ) ){
-    $display_header = false;
-}
+<?php if( $display_header && 'yes' == get_theme_mod( 'modal_toggle', 'yes' )): ?>
 
-if( $display_header ): ?>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'vi_theme' ); ?></a>
 
-<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'vi_theme' ); ?></a>
-
-<div id="modal-main-shade"></div>
-<div id="modal-main-container">
-	<div id="modal-bg"></div>
-	<div id="modal-area">
-	<?php
-		get_template_part( 'sidebar-templates/sidebar', 'modal-1' );
-		if ( has_nav_menu( 'modal' ) ) {
-			echo('<div id="nav-modal">');
-			wp_nav_menu( array(
-				'theme_location' => 'modal',
-			) );
-			echo('</div>');
-		}
-		get_template_part( 'sidebar-templates/sidebar', 'modal-2' );
-	?>
+	<div id="modal-main-shade"></div>
+	<div id="modal-main-container">
+		<div id="modal-bg"></div>
+		<div id="modal-area">
+		<?php
+			get_template_part( 'sidebar-templates/sidebar', 'modal-1' );
+			if ( has_nav_menu( 'modal' ) ) {
+				echo('<div id="nav-modal">');
+				wp_nav_menu( array(
+					'theme_location' => 'modal',
+				) );
+				echo('</div>');
+			}
+			get_template_part( 'sidebar-templates/sidebar', 'modal-2' );
+		?>
+		</div>
 	</div>
-</div>
-<div id="modal-button">MENU<a class="toggle-closed"><i class="fa fa-bars" aria-hidden="true"></i></a><a class="toggle-open"><i class="fa fa-times" aria-hidden="true"></i></a></div>
+	<div id="modal-button">MENU<a class="toggle-closed"><i class="fa fa-bars" aria-hidden="true"></i></a><a class="toggle-open"><i class="fa fa-times" aria-hidden="true"></i></a></div>
 
 <?php endif; //if display_header ?>
 
@@ -65,45 +65,93 @@ if( $display_header ): ?>
 	<?php get_template_part( 'sidebar-templates/sidebar', 'urgent-notice-1' ); ?>
 
 	<header id="masthead" class="site-header">
+		<?php if ( !empty(get_theme_mod( 'header_image', '0' ) ) ) : ?>
+			<a class="site-logo" href="<?php echo get_bloginfo('url'); ?>">
+				<img src="<?php echo get_theme_mod( 'header_image', '0' ); ?>" / alt="<?php echo get_bloginfo('name'); ?>">
+			</a>
+		<?php endif; ?>
 		<?php get_template_part( 'sidebar-templates/sidebar', 'header-1' ); ?>
 
-		    <?php if ( has_nav_menu( 'header-1' ) ): ?>
-		    	<nav  class="primary-navigation navbar navbar-expand-md" role="navigation">
-		        <div id="nav-header-1">
-		        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-navbar-collapse-1" aria-controls="bs-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-		            <span class="navbar-toggler-icon"></span>
-		        </button>
-		        <?php if( get_theme_mod( 'site_logo_navigation', 'true' ) != 'false' && get_theme_mod( 'site_logo', '0' ) != '0' ): ?>
-		        <a class="navbar-brand" href="<?php echo get_bloginfo('url'); ?>"><img src="<?php echo get_theme_mod( 'site_logo', '0' ); ?>" / alt="<?php echo get_bloginfo('name'); ?>"></a>
-		        <?php endif; ?>
+		    <?php
+		    if ( has_nav_menu( 'header-1' ) ):
+		    	if( 'bs' == get_theme_mod( 'nav_001_type', 'bs' ) ):
+		    		?>
+			    	<nav  class="primary-navigation navbar navbar-expand-md" role="navigation">
+			        <div id="nav-header-1">
+			        <?php if( '0' != get_theme_mod( 'nav_001_image', '0' ) ): ?>
+			        <a class="navbar-brand" href="<?php echo get_bloginfo('url'); ?>"><img src="<?php echo get_theme_mod( 'nav_001_image', '0' ); ?>" / alt="<?php echo get_bloginfo('name'); ?>"></a>
+			        <?php endif; ?>
 
-		        <?php
-		            wp_nav_menu( array(
-		                'theme_location' => 'header-1',
-		                'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
-		                'container'       => 'div',
-		                'container_class' => 'collapse navbar-collapse',
-		                'container_id'    => 'bs-navbar-collapse-1',
-		                'menu_class'      => 'navbar-nav mr-auto',
-		                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-		                'walker'          => new WP_Bootstrap_Navwalker(),
-		            ) );
-		        ?>
+			        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-navbar-collapse-1" aria-controls="bs-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+			            <span class="navbar-toggler-icon"></span>
+			        </button>
 
-		        </div>
-		    	</nav><!-- .primary-navigation -->
+			        <?php
+			            wp_nav_menu( array(
+			                'theme_location' => 'header-1',
+			                'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+			                'container'       => 'div',
+			                'container_class' => 'collapse navbar-collapse',
+			                'container_id'    => 'bs-navbar-collapse-1',
+			                'menu_class'      => 'navbar-nav mr-auto',
+			                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+			                'walker'          => new WP_Bootstrap_Navwalker(),
+			            ) );
+			        ?>
+
+			        </div>
+			    	</nav><!-- .primary-navigation -->
+			    <?php elseif( 'wp' == get_theme_mod( 'nav_001_type', 'bs' ) ): ?>
+			    	<nav class="main-navigation">
+			        <div id="nav-header-1">
+			        <?php
+			            wp_nav_menu( array(
+			                'theme_location' => 'header-1',
+			            ) );
+			        ?>
+			        </div>
+			    	</nav><!-- .primary-navigation -->
+			    <?php endif; ?>
 		    <?php endif; ?>
 
-		    <?php if ( has_nav_menu( 'header-2' ) ): ?>
-		    	<nav class="main-navigation">
-		        <div id="nav-header-2">
-		        <?php
-		            wp_nav_menu( array(
-		                'theme_location' => 'header-2',
-		            ) );
-		        ?>
-		        </div>
-		    	</nav><!-- .main-navigation -->
+		    <?php if ( has_nav_menu( 'header-2' ) ):
+		    	if( 'bs' == get_theme_mod( 'nav_002_type', 'bs' ) ):
+		    		?>
+			    	<nav  class="primary-navigation navbar navbar-expand-md" role="navigation">
+			        <div id="nav-header-1">
+			        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-navbar-collapse-2" aria-controls="bs-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+			            <span class="navbar-toggler-icon"></span>
+			        </button>
+			        <?php if( '0' != get_theme_mod( 'nav_002_image', '0' ) ): ?>
+			        <a class="navbar-brand" href="<?php echo get_bloginfo('url'); ?>"><img src="<?php echo get_theme_mod( 'nav_002_image', '0' ); ?>" / alt="<?php echo get_bloginfo('name'); ?>"></a>
+			        <?php endif; ?>
+
+			        <?php
+			            wp_nav_menu( array(
+			                'theme_location' => 'header-2',
+			                'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+			                'container'       => 'div',
+			                'container_class' => 'collapse navbar-collapse',
+			                'container_id'    => 'bs-navbar-collapse-2',
+			                'menu_class'      => 'navbar-nav mr-auto',
+			                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+			                'walker'          => new WP_Bootstrap_Navwalker(),
+			            ) );
+			        ?>
+
+			        </div>
+			    	</nav><!-- .secondary-navigation -->
+			    <?php elseif( 'wp' == get_theme_mod( 'nav_002_type', 'bs' ) ): ?>
+			    	<nav class="main-navigation">
+			        <div id="nav-header-2">
+			        <?php
+			            wp_nav_menu( array(
+			                'theme_location' => 'header-2',
+			            ) );
+			        ?>
+			        </div>
+			    	</nav><!-- .secondary-navigation -->
+			    <?php endif; ?>
 		    <?php endif; ?>
 
 		<?php get_template_part( 'sidebar-templates/sidebar', 'header-2' ); ?>
@@ -116,4 +164,4 @@ if( $display_header ): ?>
 
 <?php endif; //if display_header ?>
 
-	<div id="content" class="site-content">
+<div id="content" class="site-content">
